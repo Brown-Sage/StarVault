8de489011c0b0beba5816f6c71e6b9da8fbfa72d
Aaryan <luckytak60@gmail.com>
2025-05-18 02:06:30 +0530

    Navbar,Trending : Making Initial layout

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import Trending from "../components/Trending"
 import Navbar from "../components/Navbar"
 export default function Home() {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <Trending />
+        </>
     )
 }
  
